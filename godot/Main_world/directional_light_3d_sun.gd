f7c4c894e13ae2fb8f857ecb34faba0aa3c78a81
Kyunsei <kirbyvaroujan@gmail.com>
2025-11-16 17:04:22 +0100

    Main wolrd has textures and light

---
+++ godot/Main_world/directional_light_3d_sun.gd
@@ -0,0 +1,10 @@
+extends DirectionalLight3D
+
+@export var rotation_step_degrees: float = 3     # how much to rotate each tick
+var timer := 0.0
+
+func _process(delta):
+	timer += delta
+	if timer >= 0.1:   # every 1/10 second
+		rotate_x(deg_to_rad(rotation_step_degrees))
+		timer = 0.0
