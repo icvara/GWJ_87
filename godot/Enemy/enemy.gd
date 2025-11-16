7660181efa4b331aaa6773fa16bb94a21e53d7c3
Kyunsei <kirbyvaroujan@gmail.com>
2025-11-16 18:06:40 +0100

    Tried to make the enemy die with the sun

--- godot/Enemy/enemy.gd
+++ godot/Enemy/enemy.gd
@@ -1,9 +1,31 @@
 extends CharacterBody3D
 
 @export var  target : Node3D
+@export var sun: DirectionalLight3D
+@onready var ray: RayCast3D = $CollisionShape3D/RayCast3D_suncheck
+var is_dead = false
 
 func _physics_process(delta: float) -> void:
+		if is_dead or sun == null:
+			return
+	# Aim ray towards the sun
+		var sun_dir = -sun.global_transform.basis.z
+		ray.target_position = sun_dir * 350.0  # make ray long
+		if ray.is_colliding():
+			die_from_sunlight()
+			#var obj = ray.get_collider()
+			## If something blocks the sun (like a roof), zombie survives
+			#if obj.is_in_group("SunBlocker"):
+				#return
+	## If nothing is blocking → sunlight hits zombie
+			#else :
+				#die_from_sunlight()
 		if not is_on_floor():
 				velocity.y -= WorldData.gravity * delta	
 		move_and_slide()
 		#print(velocity)
+
+func die_from_sunlight():
+	#is_dead = true
+	print("Zombie died from sunlight!")
+	#queue_free()
--- godot/Main_world/directional_light_3d_sun.gd
+++ godot/Main_world/directional_light_3d_sun.gd
@@ -2,6 +2,7 @@ extends DirectionalLight3D
 
 @export var rotation_step_degrees: float = 3     # how much to rotate each tick
 var timer := 0.0
+var sun_direction = -global_transform.basis.z
 
 func _process(delta):
 	timer += delta
