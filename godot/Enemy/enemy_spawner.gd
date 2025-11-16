8a1581fe6549bf917393d05f9dbb96a16de7b7a1
Kyunsei <kirbyvaroujan@gmail.com>
2025-11-16 20:17:50 +0100

    dies better spawn better raycast, still issues with spawn

--- godot/Enemy/enemy_spawner.gd
+++ godot/Enemy/enemy_spawner.gd
@@ -4,20 +4,22 @@ extends Node3D
 @export var enemy1 :PackedScene
 @export var target :Node3D
 @export var sun: Node3D
-
+@onready var ray: RayCast3D = $RayCast3D_suncheck
 
 
 var timer = 1
 
 
 func _process(delta: float) -> void:
-	if WorldData.isNight != 0 :
+	ray.target_position = sun.global_position #* 350.0  # make ray long
+	if ray.is_colliding():
 		timer -= delta
-		if timer <=0 : 
+		if timer <= 0.0:
 			Spawn(1)
-			timer = randi_range(2,3)
+			reset_timer()  # Reset timer after each spawn
 
-
+func reset_timer():
+	timer = randf_range(2.0, 3.0)  # rand_range returns a float
 
 func Spawn(id):
 	if id == 1:
