70721e01d77b94777a866c68765cb1fd45a30d84
Kyunsei <kirbyvaroujan@gmail.com>
2025-11-16 21:26:18 +0100

    even more hardcore

--- godot/Main_world/sun.gd
+++ godot/Main_world/sun.gd
@@ -6,7 +6,7 @@ extends Node3D
 
 var center = Vector3(0, 0,0)
 var radius = 45
-var speed = 0.1   # radians per second
+var speed = 0.01   # radians per second
 var angle = 0.0
 
 func _process(delta: float) -> void:
