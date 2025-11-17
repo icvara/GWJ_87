c05506fc375b36baa77236ab4672dc10f35a1b55
Icvara <icvara.aor@gmail.com>
2025-11-17 01:23:50 +0000

    add a win

---
+++ godot/Main_world/win_condition.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+var count = 4
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.name == "Player2D3D":
+		count -= 1
+		$CanvasLayer/Panel/Label.text = "Bring " + str(count) + " objects"
+		if count <0 :
+			$CanvasLayer/Panel/Label.text = "You Win"
+			$CanvasLayer.show()
+			get_tree().paused = true
+
+		$CanvasLayer.show()
+		
+
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.name == "Player2D3D":
+		$CanvasLayer.hide()
