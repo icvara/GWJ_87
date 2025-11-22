3835fc3d0f649f5fb7f8ba454d7de4a0657e47a0
Kyunsei <kirbyvaroujan@gmail.com>
2025-11-22 17:06:58 +0100

    Better campfire

--- godot/3d_asset/camp_fire.gd
+++ godot/3d_asset/camp_fire.gd
@@ -1,20 +1,33 @@
 extends Node3D
 
 
-var HP_campfire = 100
-var maxHP_campfire = 100
-var health_decrease_rate := 5.0  # HP per second
+var HP_campfire = 100.0
+var maxHP_campfire = 100.0
+var health_decrease_rate = 20.0  
+var time_before_decay = 2.0
+var timer = 0.0
+var max_brightness = 5.0
+var min_brightness = 0.2
+var flicker_amount = 0.2
+var speed = 0.5
 
 func _ready():
 	pass
 	
 func _physics_process(delta: float) -> void:
-	if HP_campfire > 0:
-		HP_campfire -= health_decrease_rate * delta
-		HP_campfire = max(HP_campfire, 0)  # prevent going below 0
-		print(HP_campfire)
+	timer +=delta
+	if time_before_decay <= timer:
+		if HP_campfire > 0:
+			HP_campfire -= health_decrease_rate 
+			HP_campfire = max(HP_campfire, 0)  # prevent going below 0
+			print(HP_campfire)
+			timer = 0
 	if HP_campfire <0 or HP_campfire == 0:
 		death()
 		
+	var health_ratio = HP_campfire / maxHP_campfire
+	$OmniLight3D.light_energy = lerp(min_brightness, max_brightness, health_ratio)+ sin(Time.get_ticks_msec() * 0.01 * speed) * flicker_amount *randf_range(1,1.5)
+	#self.omni_range = 2 + lerp(min_brightness, max_brightness, health_ratio)
+
 func death() :
 	queue_free()
