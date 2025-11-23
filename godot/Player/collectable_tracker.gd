ea022a0a6aec745c9a47ba3c7929c17bc07b4a73
Kyunsei <kirbyvaroujan@gmail.com>
2025-11-23 17:58:11 +0100

    hud better

--- godot/Player/collectable_tracker.gd
+++ godot/Player/collectable_tracker.gd
@@ -4,11 +4,8 @@ extends Label
 func _process(delta: float) -> void:
 	if str(get_parent().get_parent().item) == "20":
 		modulate = Color(1,0,0)
-		self.scale = Vector2(2,2)
-		print(self)
-		text = "You have 20/20 items, get it back to the Sunflower!"
+		text = "You have 20/20 items!"
 	else :
 		modulate = Color(1,1,1)
 		self.scale = Vector2(1,1)
-		print(self)
 		text = "You have "  + str(get_parent().get_parent().item) + "/20 Items"
