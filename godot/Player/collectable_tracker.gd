95d5b71b70952bf8db1e00d8e64d16ac5d850ee5
Icvara <icvara.aor@gmail.com>
2025-11-23 18:01:33 +0000

    HUD_sunflower

--- godot/Player/collectable_tracker.gd
+++ godot/Player/collectable_tracker.gd
@@ -5,7 +5,12 @@ func _process(delta: float) -> void:
 	if str(get_parent().get_parent().item) == "20":
 		modulate = Color(1,0,0)
 		text = "You have 20/20 items!"
+		$AnimationPlayer.play("new_animation")
+
 	else :
 		modulate = Color(1,1,1)
 		self.scale = Vector2(1,1)
-		text = "You have "  + str(get_parent().get_parent().item) + "/20 Items"
+		#$AnimationPlayer.play("new_animation")
+		$AnimationPlayer.stop()
+
+	text = "["  + str(get_parent().get_parent().item) + "/20]"
