3177743693cd39a6b7e9b8e301a6d77860141e38
Kyunsei <kirbyvaroujan@gmail.com>
2025-11-23 17:12:04 +0100

    color for HUD

--- godot/Player/collectable_tracker.gd
+++ godot/Player/collectable_tracker.gd
@@ -8,6 +8,7 @@ func _process(delta: float) -> void:
 		print(self)
 		text = "You have 20/20 items, get it back to the Sunflower!"
 	else :
+		modulate = Color(1,1,1)
 		self.scale = Vector2(1,1)
 		print(self)
 		text = "You have "  + str(get_parent().get_parent().item) + "/20 Items"
