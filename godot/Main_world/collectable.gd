317295d545153cba958a164b1bb51b1667aed565
Kyunsei <kirbyvaroujan@gmail.com>
2025-11-23 13:23:20 +0100

    item is capped at 20 on player.

--- godot/Main_world/collectable.gd
+++ godot/Main_world/collectable.gd
@@ -7,8 +7,10 @@ extends RigidBody3D
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	#print(body.name)
 	if body.name == "Player2D3D":
-		body.item += 1
+		if body.item < body.max_item :
+			body.item += 1
 		body.HP = clamp(body.HP+15,0.,100.)
 		body.get_node("music").get_node("Collect_object").play()
 		queue_free()
+
 		
--- godot/Player/player_2d_3d.gd
+++ godot/Player/player_2d_3d.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 var is_screen_focus = true
 var win = false
 var item = 0
+var max_item = 20
 @export var plant1 : PackedScene
 @export var plant2 : PackedScene
 @export var plant3 : PackedScene
